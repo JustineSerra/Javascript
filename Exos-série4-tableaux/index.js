1c4eaa30eeda062d88e7265a4ed52b302f69a795
Justine Serra <bridel44@hotmail.fr>
2026-05-19 16:54:38 +0200

    en cours

--- Exos-série4-tableaux/index.js
+++ Exos-série4-tableaux/index.js
@@ -6,3 +6,12 @@ const nombres = [1,2,3,4,5];
 */
 
 /*Exercice 2*/
+function sommeTableau(tableau) {
+    let somme = 0;
+    for (let nombre of tableau) {
+        somme += nombre;
+    }
+    return somme;
+}
+const Nombres=[2,5,10,14];
+console.log(sommeTableau(Nombres));
