669981f913ad44741d4dea2e325e7f536980c2da
Justine Serra <bridel44@hotmail.fr>
2026-05-27 12:16:01 +0200

    terminé

--- DOM-exos/exo9.js
+++ DOM-exos/exo9.js
@@ -6,8 +6,41 @@ let produits = [
     {nom:"Gomme", prix:0.8},
 ];
 
-let table=document.createDocumentFragment("table");
+let table=document.createElement("table");
+//création de l'éléement table principal 
+let entete=document.createElement("tr");
+//création de la ligne de l'entête
 
-//appendChild(document.createElement());
+let thNom=document.createElement("th");
+thNom.textContent="Nom";
+entete.appendChild(thNom);
+//création de la première case d'entête
+
+let thPrix=document.createElement("th");
+thPrix.textContent="Prix";
+entete.appendChild(thPrix);
+table.appendChild(entete);
+//création de la deuxieme case d'entête
+
+produits.forEach(produit => {
+//on parcout le tableau de produits pour créer une ligne par élément
+    let ligne=document.createElement("tr");
+    //nouvelle ligne (tr) pour le produit actuel
+    let tdNom=document.createElement("td");
+    //case pour le nom
+    tdNom.textContent=produit.nom;
+    ligne.appendChild(tdNom); //on l'ajoute à la ligne
+
+    let tdPrix=document.createElement("td");
+    //création de la case pour le prix
+    tdPrix.textContent=produit.prix;
+    ligne.appendChild(tdPrix);
+
+
+    table.appendChild(ligne);
+    //on ajoute la ligne complète au tableau
+});
+document.body.appendChild(table);
+//on injecte tout le tableau dans le body de la page
 
 */
